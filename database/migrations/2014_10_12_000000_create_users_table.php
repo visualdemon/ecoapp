133b46dc206f3d7130aaf3b6c8bcab783475bd15
Wilber Jurado <wilberamp@gmail.com>
2023-11-08 00:06:24 -0500

    Filament listo para iniciar con los modelos

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+
+            // Campos adicionales
+            $table->string('last_name');
+            $table->string('cellphone');
+            $table->string('identification_number');
+            $table->date('date_of_birth');
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
---
+++ database/migrations/2023_11_08_041153_create_contributions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contributions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('file_count');
+            $table->decimal('value', 8, 2);
+            $table->unsignedBigInteger('units_id');
+            $table->foreign('units_id')->references('id')->on('measurement_units');
+            $table->foreignId('customer_storage_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contributions');
+    }
+};
